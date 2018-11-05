44cc8603dfc8b30a60e4407438c4ed61ddadccd0
zintags <zintags@gmail.com>
2018-11-05 21:40:46 +0900

    init commit

---
+++ src/main/java/com/helloworld/demohelloworld/DemohelloworldApplication.java
@@ -0,0 +1,12 @@
+package com.helloworld.demohelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohelloworldApplication.class, args);
+    }
+}
---
+++ src/test/java/com/helloworld/demohelloworld/DemohelloworldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.helloworld.demohelloworld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemohelloworldApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
